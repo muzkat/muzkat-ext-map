497d280fbc1f6e969fb62d8391a39f2d6ba3ba48
muzkat <erik@muzkat.com>
2018-01-26 22:05:59 +0100

    Improvements

--- public/js/muzkatmap.debug.js
+++ public/js/muzkatmap.debug.js
@@ -503,3 +503,11 @@ Ext.define('muzkatMap.muzkatMap', {
         this.callParent(arguments);
     }
 });
+
+Ext.define('muzkatMap.muzkatMapWidget', {
+    extend: 'muzkatMap.muzkatMap',
+    alias: 'widget.muzkatMapWidget',
+
+    header: true,
+    hideDetails: true
+});
